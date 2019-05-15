54720d0cd29bcd9f4f1f0f9a189d00c269198d69
Takuya Koriyama <takkuntakuya@icloud.com>
2019-05-15 11:46:13 +0900

    Network Module

--- tutorials_4.py
+++ tutorials_4.py
@@ -6,21 +6,6 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 
-transform = transforms.Compose(
-    [transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
-)
-
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
-
-testset = torchvision.datasets.CIFAR10(root='./data', train=True, download=False, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=True, num_workers=2)
-
-
-classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-
-
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
