a78dcc95e8109fd39e084d3aa453016a73a0d991
Takuya Koriyama <takkuntakuya@icloud.com>
2019-05-15 11:48:32 +0900

    scores of the models

--- tutorials_4_test.py
+++ tutorials_4_test.py
@@ -13,12 +13,14 @@ images, labels = dataiter.next()
 
 imshow(torchvision.utils.make_grid(images))
 print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
-
 outputs = net(images)
-
 _, predicted = torch.max(outputs, 1)
-
 print('Predicted: ', ' '.join('%5s' % classes[predicted[j]] for j in range(4)))
+
+
+
+
+
 
 correct = 0
 total = 0
@@ -29,20 +31,23 @@ with torch.no_grad():
         outputs = net(images)
         _, predicted = torch.max(outputs.data, 1)
         total += labels.size(0)
-        correct += (predicted == labels).sum().items()
+        correct += (predicted == labels).sum().item()
 
 print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct/ total))
+
+
+
+
 
 
 class_correct = list(0. for i in range(10))
 class_total = list(0. for i in range(10))
 
-
 with torch.no_grad():
         for data in testloader:
                 images, labels = data
                 outputs = net(images)
-                 _, predicted = torch.max(outputs, 1)
+                _, predicted = torch.max(outputs, 1)
                 c = (predicted == labels).squeeze()
                 for i in range(4):
                         label = labels[i]
