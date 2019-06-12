e95f69feadb6100a63d7c69402a43067d22d8a08
Takuya Koriyama <takkuntakuya@icloud.com>
2019-06-12 15:53:22 +0900

    add argsparse

--- fine_tune.py
+++ fine_tune.py
@@ -8,17 +8,52 @@ import time
 import os
 import copy
 from finetune_helper_function import train_model, set_parameter_requires_grad, initialize_model
+import argparse
+import pickle
+
+parser = argparse.ArgumentParser(description="recognizer")
 
 
+parser.add_argument("--pretrained_model_name", type = str, default='alexnet', 
+    choices=["resnet", "alexnet", "vgg", "squeezenet", "densenet", "inception"])
+parser.add_argument("--new_model_name", type=str, required=True)
+parser.add_argument("--num_classes", type=int, default=2)
+parser.add_argument("--feature_extract", action="store_true")
+parser.add_argument("--data_dir", type=str, required=True, help="ex) ./img/animals")
+parser.add_argument("--bacth_size", type=int, default=4)
+parser.add_argument("--num_epochs", type=int, default=10)
+parser.add_argument("--class_name", type=str, nargs="*", required=True)
+parser.add_argument("--lr", type=float, default = 0.001)
+parser.add_argument("--momentum", type=float, default=0.9)
+parser.add_argument('--save_model', action='store_true', default=False)
 
-model_name = "alexnet"
-num_classes = 2
-feature_extract = True
-data_dir = "./img/hymenoptera_data"
-bacth_size = 4
-num_epochs = 15
+args = parser.parse_args()
+
+model_name = args.pretrained_model_name
+new_model_name = args.new_model_name
+new_model_path = "./model/" + new_model_name + ".pkl"
+new_model_label_path = "./label_to_name/" + new_model_name + ".label_to_name"
+new_model_hist_path = "./hist/" + new_model_name + ".hist"
+num_classes = args.num_classes
+feature_extract = args.feature_extract
+data_dir = args.data_dir
+bacth_size = args.bacth_size
+num_epochs = args.num_epochs
+label_to_name = args.class_name
+lr = args.lr
+momentum = args.momentum
+save_model = args.save_model
 
 model, input_size = initialize_model(model_name, num_classes, feature_extract, use_pretrained=True)
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+model = model.to(device)
+params_to_update = model.parameters()
+optimizer = optim.SGD(params_to_update, lr=lr, momentum=momentum)
+criterion = nn.CrossEntropyLoss()
+
+
+
 
 data_transforms = {
 'train': transforms.Compose([
@@ -35,13 +70,12 @@ transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ]),
 }
 print("Initializing Datasets and Dataloaders...")
-
 image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x]) for x in ['train', 'val']}
 dataloaders_dict = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=bacth_size, shuffle=True, num_workers=4) for x in ['train', 'val']}
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-model = model.to(device)
-params_to_update = model.parameters()
+
+
+
 print("Params to learn:")
 
 if feature_extract:
@@ -58,9 +92,23 @@ else:
 
 
 
-optimizer = optim.SGD(params_to_update, lr=0.001, momentum=0.9)
-criterion = nn.CrossEntropyLoss()
+
 
 model, hist = train_model(model, dataloaders_dict, criterion, optimizer, num_epochs=num_epochs, is_inception=(model_name=="inception"))
 
-torch.save(model, "./model/ant_bee_classifier.pkl")
+if save_model:
+    torch.save(model, new_model_path) 
+    
+    f = open(new_model_label_path, 'wb')
+    pickle.dump(label_to_name, f)
+
+    f = open(new_model_hist_path, 'wb')
+    pickle.dump(hist, f)
+
+else:
+    pass
+
+
+####改善点###
+#label_to_nameを専用のフォルダを作ってそこに保存して、recognizeするときにlaodするという手法を取っているが、
+# そうではなくて、モデルにlabel_to_nameを付与するようなcodeを描きたい
--- recognize_img.py
+++ recognize_img.py
@@ -1,24 +1,33 @@
 from finetune_helper_function import train_model, set_parameter_requires_grad, initialize_model
 from cv2_helper_function import read_img, vis_img, transform, recognize, cv2pil
 import torch
+import argparse
+import pickle
+
+parser = argparse.ArgumentParser(description="recognizer")
+
+parser.add_argument("--model_path", required=True)
+parser.add_argument("--img_path", required=True)
+parser.add_argument("--label_to_name_path", required=True)
+args = parser.parse_args()
 
 
-model = torch.load("./model/ant_bee_classifier.pkl")
-
-label_to_name = ["ants", "bees"]
-
-img_path = "./img/test_img/ants.jpg"
+model_path = args.model_path
+label_to_name_path = args.label_to_name_path
+img_path = args.img_path
 
 
+
+model = torch.load(model_path)
+f = open(label_to_name_path, 'rb')
+label_to_name = pickle.load(f)
 img_cv = read_img(img_path=img_path)
 vis_img(img=img_cv)
-
 img_pil = cv2pil(img_cv)
 img = transform(img_pil)
 outputs = recognize(model=model, img=img)
 
 _, pred = torch.max(outputs, 1)
-
 
 print(label_to_name[pred])
 
