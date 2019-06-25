ce922ba4a698cb8c12941ed0a191f24cb95393a6
Takuya Koriyama <takkuntakuya@icloud.com>
2019-06-25 20:14:13 +0900

    add detection

---
+++ detection/segmentation.py
@@ -0,0 +1,30 @@
+import torchvision
+import transform as T
+model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
+
+img = 
+
+
+Host aws
+    HostName XXX.XXX.XXX.XXX
+    IdentityFile YOUR/KEY/PATH/monopo_share.pem
+    User ubuntu
+
+
+Host aws
+    HostName ec2-52-10-1-0.us-west-2.compute.amazonaws.com
+    IdentityFile /Users/kooriyamatakuya/CV/urop/monopo_share.pem
+    User ubuntu
+
+
+sudo apt update
+
+sudo apt upgrade
+
+htop 
+
+nvidia-smi
+
+git pull origin master
+
+pip list | grep torch
