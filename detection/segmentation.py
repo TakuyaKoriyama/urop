784b783066c723986bf8cd3a9bafb1a7024a840e
Takuya Koriyama <takkuntakuya@icloud.com>
2019-06-26 22:51:23 +0900

    modified segmentation.py

--- detection/segmentation.py
+++ detection/segmentation.py
@@ -1,30 +1,61 @@
 import torchvision
-import transform as T
+from torchvision.transforms import functional as F
+import cv2
+# from cv2_helper_function import cv2pil
+from PIL import Image
+def cv2pil(image_cv):
+    image_cv = cv2.cvtColor(image_cv, cv2.COLOR_BGR2RGB)
+    image_pil = Image.fromarray(image_cv)
+    image_pil = image_pil.convert('RGB')
+
+    return image_pil
+
 model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
+model.eval()
+label_to_name = [
+    '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
+    'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A', 'stop sign',
+    'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
+    'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack', 'umbrella', 'N/A', 'N/A',
+    'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+    'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
+    'bottle', 'N/A', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
+    'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+    'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table',
+    'N/A', 'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
+    'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A', 'book',
+    'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush'
+]
 
-img = 
+cap = cv2.VideoCapture(0)
+
+while True:
+    
+    ret, frame = cap.read()
+
+    img = cv2pil(frame)
+    img = F.to_tensor(img)
+    pred = model(img)
+
+    boxes = pred["boxes"]
+    labels = pred["labels"]
+    scores = pred["scores"]
+    masks = pred["masks"]
 
 
-Host aws
-    HostName XXX.XXX.XXX.XXX
-    IdentityFile YOUR/KEY/PATH/monopo_share.pem
-    User ubuntu
 
 
-Host aws
-    HostName ec2-52-10-1-0.us-west-2.compute.amazonaws.com
-    IdentityFile /Users/kooriyamatakuya/CV/urop/monopo_share.pem
-    User ubuntu
+    edframe = frame
+
+    cv2.putText(edframe, label_to_name[labels], (0,50), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255,0), 3, cv2.LINE_AA)
 
 
-sudo apt update
+    cv2.imshow('Edited Frame', edframe)
 
-sudo apt upgrade
+    k = cv2.waitKey(1)
+    if k == 27:
+        break
 
-htop 
 
-nvidia-smi
-
-git pull origin master
-
-pip list | grep torch
+cap.release()
+cv2.destroyAllWindows()
