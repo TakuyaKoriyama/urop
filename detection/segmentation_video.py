a487cf41b80bb152ab6714cc913ab5468734c445
Takuya Koriyama <takkuntakuya@icloud.com>
2019-07-03 23:20:29 +0900

    add IOU

--- detection/segmentation_video.py
+++ detection/segmentation_video.py
@@ -1,17 +1,21 @@
 import torchvision
 from torchvision.transforms import functional as F
 import cv2
-from transforms import cv2pil
+from transforms import cv2pil, IOU_cheack
 import time
 import argparse
 
 parser = argparse.ArgumentParser(description='segmentation using webcamera')
-parser.add_argument('base_model', choices=['faster_rcnn', 'mask_rcnn', 'keypoint_rcnn'])
+parser.add_argument('-frame_num', default=20)
+parser.add_argument('-score_thr', default=0.5)
+parser.add_argument('-iou_thr', default=0.5)
 args = parser.parse_args()
 
-base_model = args.base_model
+frame_num = args.frame_num
+score_thr = args.score_thr
+iou_thr = args.iou_thr
 
-label_to_name_1 = [
+label_to_name = [
     '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
     'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A', 'stop sign',
     'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
@@ -26,124 +30,55 @@ label_to_name_1 = [
     'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush'
 ]
 
-label_to_name_2 = ['__background__', 'person']
-keypoint_label = [
-    'nose',
-    'left_eye',
-    'right_eye',
-    'left_ear',
-    'right_ear',
-    'left_shoulder',
-    'right_shoulder',
-    'left_elbow',
-    'right_elbow',
-    'left_wrist',
-    'right_wrist',
-    'left_hip',
-    'right_hip',
-    'left_knee',
-    'right_knee',
-    'left_ankle',
-    'right_ankle'
-]
-if base_model != 'keypoint_rcnn':
-    if base_model == 'faster_rcnn':
-        model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
-    else:
-        model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
+model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+model.eval()
+cap = cv2.VideoCapture(0)
+time_sum = 0
+for i in range(frame_num):
+    start = time.time()
 
-    model.eval()
-    cap = cv2.VideoCapture(0)
-
-    time_sum = 0
+    ret, frame = cap.read()
+    img = cv2pil(frame)
+    img = F.to_tensor(img)
     
-    for i in range(20):
-        start = time.time()
-
-        ret, frame = cap.read()
-        img = cv2pil(frame)
-        img = F.to_tensor(img)
-        
-        
-        pred = model([img])
-        pred = pred[0]
-        object_num = len(pred['labels'])
-        
-        
-
-        for i in range(object_num):
-
-            box = pred['boxes'][i]
-            label = pred['labels'][i]
-            score = pred['scores'][i]
-            
-            if score < 0.5:
-                pass
-            else:    
-                cv2.rectangle(frame, (box[0], box[1]), (box[2], box[3]), (0, 0, 255), 5)
-                cv2.putText(frame, label_to_name_1[label] + " acc: {}%".format(int(score*100)), (box[0],box[1]), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255,0), 3, cv2.LINE_AA)
-                
-        
-        cv2.imshow('Detection frame', frame)        
-        elapsed_time = time.time() - start
-        time_sum += elapsed_time
-
-        k = cv2.waitKey(1)
-        
-        if k == 27:
-            break
-
-else:
-    model = torchvision.models.detection.keypointrcnn_resnet50_fpn(pretrained=True)
-    model.eval()
-    cap = cv2.VideoCapture(0)
-
-    time_sum = 0
     
-    for i in range(20):
-        start = time.time()
+    pred = model([img])[0]
+    boxes = pred['boxes']
+    labels = pred['labels']
+    scores = pred['scores']
+    object_num = len(labels)
 
-        ret, frame = cap.read()
-        img = cv2pil(frame)
-        img = F.to_tensor(img)
+    high_score_id = []
+
+    for i in range(object_num):
+        score = scores[i]
+        if score < score_thr:
+            pass
+        else:
+            high_score_id.append(i)
+    
+    for i in high_score_id:
+        for j in high_score_id:
+            if not IOU_cheack(boxes[i], boxes[j]):
+                break
+            else:
+                continue
+        box = boxes[i]
+        label = labels[i]
+        score = scores[i]
+        cv2.rectangle(frame, (box[0], box[1]), (box[2], box[3]), (0, 0, 255), 5)
+        cv2.putText(frame, label_to_name[label] + " {}%".format(int(score*100)), (box[0],box[1]), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255,0), 3, cv2.LINE_AA)
         
-        
-        pred = model([img])
-        pred = pred[0]
-        object_num = len(pred['labels'])
-        
-        
-        
 
-        for i in range(object_num):
-            box = pred['boxes'][i]
-            label = pred['labels'][i]
-            score = pred['scores'][i]
-            keypoint = pred['keypoints'][i]
-            keypoint_num = len(keypoint)
-            print(keypoint.size(), 'keypoint_size')
-            print(keypoint, 'keypoint')
+    cv2.imshow('Detection frame', frame)        
+    elapsed_time = time.time() - start
+    time_sum += elapsed_time
 
-            if score < 0.5:
-                pass
-            else:    
-                cv2.rectangle(frame, (box[0], box[1]), (box[2], box[3]), (0, 0, 255), 5)
-                cv2.putText(frame, label_to_name_2[label] + " acc: {}%".format(int(score*100)), (box[0],box[1]), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255,0), 3, cv2.LINE_AA)
-                for j in range(keypoint_num):
-                    key_part = keypoint[j]
-                    print(key_part.size(), 'key_part_size')
-                    print(key_part, 'keypart') ##これがおそらくベクトル。
-        cv2.imshow('Detection frame', frame)        
-        elapsed_time = time.time() - start
-        time_sum += elapsed_time
+    k = cv2.waitKey(1)
+    
+    if k == 27:
+        break
 
-        k = cv2.waitKey(1)
-        
-        if k == 27:
-            break
-
-
-print(time_sum/20)
-
+print(time_sum/frame_num)
 cap.release()
 cv2.destroyAllWindows()
