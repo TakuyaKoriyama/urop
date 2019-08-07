2d7c6970f9cdb986fb7f4db886214b2c6f1a9ee4
TakuyaKoriyama <takkuntakuya@g.ecc.ac.jp>
2019-08-07 17:29:06 +0900

    remove unnecessary coment

--- detection/finetune.py
+++ detection/finetune.py
@@ -70,13 +70,8 @@ class Dataset(object):
         return len(self.imgs)
 
 def get_model_instance_segmentation(num_classes):
-    # load a model pre-trained pre-trained on COCO
     model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
-    # replace the classifier with a new one, that has
-    # num_classes which is user-defined
-    # get number of input features for the classifier
     in_features = model.roi_heads.box_predictor.cls_score.in_features
-    # replace the pre-trained head with a new one
     model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
     return model
 
